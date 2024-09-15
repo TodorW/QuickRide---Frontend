8531bbe25fc63959b7c3a845b60b02517ea70332
Vuk Todorović <vuk.todorovic@sss-nk.edu.me>
2024-09-15 15:23:23 +0200

    added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './auth/Login.js';
+import Signup from './auth/Signup.js';
+import Logout from './auth/Logout.js';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/" element={<h1>Welcome to the Home Page</h1>} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
