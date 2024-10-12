1ddbee204857838d3e724ff420b8be8df022c7ce
Vuk Todorović <vuk.todorovic@sss-nk.edu.me>
2024-10-12 21:33:19 +0200

    modified input fields on confirm reservation

--- src/components/confirm-reservation/ConfirmReservation.js
+++ src/components/confirm-reservation/ConfirmReservation.js
@@ -55,7 +55,7 @@ const ConfirmReservation = ({ open, setOpen }) => {
     >
       <DialogBackdrop
         transition
-        className="fixed inset-0 hidden bg-gray-500 bg-opacity-75 dark:bg-black dark:bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in md:block"
+        className="fixed inset-0 hidden bg-gray-900 bg-opacity-75 dark:bg-black dark:bg-opacity-75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in md:block"
       />
 
       <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
@@ -64,7 +64,7 @@ const ConfirmReservation = ({ open, setOpen }) => {
             transition
             className="flex w-full transform text-left text-base transition data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in md:my-8 md:max-w-2xl md:px-4 data-[closed]:md:translate-y-0 data-[closed]:md:scale-95 lg:max-w-4xl"
           >
-            <div className="relative flex w-full items-center overflow-hidden bg-white dark:bg-gray-800 px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
+            <div className="relative flex w-full items-center overflow-hidden bg-white dark:bg-gray-900 px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8">
               <button
                 type="button"
                 onClick={() => setOpen(false)}
@@ -75,7 +75,7 @@ const ConfirmReservation = ({ open, setOpen }) => {
               </button>
 
               <div className="grid w-full grid-cols-1 items-start gap-x-6 gap-y-8 sm:grid-cols-12 lg:gap-x-8">
-                <div className="aspect-h-3 aspect-w-2 overflow-hidden rounded-lg bg-gray-100 dark:bg-gray-700 sm:col-span-4 lg:col-span-5">
+                <div className="aspect-h-3 aspect-w-2 overflow-hidden rounded-lg bg-gray-100 dark:bg-gray-900 sm:col-span-4 lg:col-span-5">
                   <img
                     alt={`Car image`}
                     src={`http://tim1.cortexakademija.com/storage/cars-images/${reservation.car.image}`}
@@ -83,66 +83,66 @@ const ConfirmReservation = ({ open, setOpen }) => {
                   />
                 </div>
                 <div className="sm:col-span-8 lg:col-span-7">
-                  <h2 className="text-2xl font-bold text-gray-900 dark:text-gray-100 sm:pr-12">
-                    <p>
-                      Car: {reservation.car.make} {reservation.car.model}
-                    </p>
-                  </h2>
+                <h2 className="text-2xl font-bold text-gray-400 dark:text-gray-400 sm:pr-12">
+  <p>
+    Car: {reservation.car.make} {reservation.car.model}
+  </p>
+</h2>
 
-                  <section
-                    aria-labelledby="information-heading"
-                    className="mt-2"
-                  >
-                    <h3 id="information-heading" className="sr-only">
-                      Car information
-                    </h3>
+<section
+  aria-labelledby="information-heading"
+  className="mt-2"
+>
+  <h3 id="information-heading" className="sr-only">
+    Car information
+  </h3>
 
-                    <p className="text-2xl text-gray-900 dark:text-gray-100">
-                      Total price: ${reservation.totalPrice}
-                    </p>
-                  </section>
+  <p className="text-2xl text-gray-400 dark:text-gray-400">
+    Total price: ${reservation.totalPrice}
+  </p>
+</section>
 
-                  <section aria-labelledby="options-heading" className="mt-10">
-                    <h3 id="options-heading" className="sr-only">
-                      Product options
-                    </h3>
-                    <div className="mb-4">
-                      <label
-                        htmlFor="start_date"
-                        className="block mb-2 text-sm font-medium"
-                      >
-                        Start date:
-                      </label>
-                      <DatePicker
-                        id="start_date"
-                        selected={reservation.startDate}
-                        className="block w-full px-4 py-2 bg-gray-300 border border-gray-300 rounded-md text-gray-900"
-                        placeholderText="Start date"
-                        showTimeSelect
-                        dateFormat="yyyy-MM-dd HH:mm:ss"
-                        readOnly
-                        disabled
-                      />
-                    </div>
+<section aria-labelledby="options-heading" className="mt-10">
+  <h3 id="options-heading" className="sr-only">
+    Product options
+  </h3>
+  <div className="mb-4">
+  <label
+    htmlFor="start_date"
+    className="block mb-2 text-sm font-medium text-gray-400"
+  >
+    Start date:
+  </label>
+  <DatePicker
+    id="start_date"
+    selected={reservation.startDate}
+    className="block w-full px-4 py-2 bg-gray-800 border border-gray-700 rounded-md text-gray-400" // Dodata klasa za svetlo sivu boju
+    placeholderText="Start date"
+    showTimeSelect
+    dateFormat="yyyy-MM-dd HH:mm:ss"
+    readOnly
+    disabled
+  />
+</div>
 
-                    <div className="mb-4">
-                      <label
-                        htmlFor="end_date"
-                        className="block mb-2 text-sm font-medium"
-                      >
-                        End date:
-                      </label>
-                      <DatePicker
-                        id="end_date"
-                        selected={reservation.endDate}
-                        className="block w-full px-4 py-2 bg-gray-300 border border-gray-300 rounded-md text-gray-900"
-                        placeholderText="End date"
-                        showTimeSelect
-                        dateFormat="yyyy-MM-dd HH:mm:ss"
-                        readOnly
-                        disabled
-                      />
-                    </div>
+<div className="mb-4">
+  <label
+    htmlFor="end_date"
+    className="block mb-2 text-sm font-medium text-gray-400"
+  >
+    End date:
+  </label>
+  <DatePicker
+    id="end_date"
+    selected={reservation.endDate}
+    className="block w-full px-4 py-2 bg-gray-800 border border-gray-700 rounded-md text-gray-400" // Dodata klasa za svetlo sivu boju
+    placeholderText="End date"
+    showTimeSelect
+    dateFormat="yyyy-MM-dd HH:mm:ss"
+    readOnly
+    disabled
+  />
+</div>
 
                     <form>
                       <button
