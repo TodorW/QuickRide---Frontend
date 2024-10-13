8b9797f97c221c422370623d45bea4d2d9f75820
Luka <lukamasulovic2@gmail.com>
2024-10-13 17:51:48 +0200

    refactoring redux

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import profileReducer from "./profileSlice";
 import reservationReducer from "./reservationSlice";
 export const store = configureStore({
   reducer: {
-    profile: profileReducer,
     reservation: reservationReducer,
   },
 });
